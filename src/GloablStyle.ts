98ade02e8f5222f3fa576391e6fd655e473e4a2c
YeongG <yeonggil223@gmail.com>
2021-05-25 23:46:23 +0900

    [CREATE] GlobalStyle

---
+++ src/GloablStyle.ts
@@ -0,0 +1,13 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        margin:0;
+        padding:0;
+        font-family:"Noto Sans KR", sans-serif !important;
+    }
+`;
+
+export const GlobalBackground = styled.div`
+  background-color: rgb(246, 246, 246);
+`;
