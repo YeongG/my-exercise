622f4f4e3d7165d62f56e542d2e471667791b6a4
YeongG <yeonggil223@gmail.com>
2021-05-26 23:49:33 +0900

    [MODIFY] GlobalStyle fadeOut, slideUp 추가

--- src/GloablStyle.ts
+++ src/GloablStyle.ts
@@ -1,10 +1,33 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+
+export const fadeOut = keyframes`
+  0% {}
+  100% {
+    transform:scale(1.1);
+    opacity:0;
+  }
+`;
+
+export const SlideUp = (px: number) => keyframes`
+  0% {
+    opacity:0;
+    transform:translateY(${px}px);
+  }
+  100% {
+    opacity:1;
+    transform:translateY(0px);
+  }
+`;
 
 export const GlobalStyle = createGlobalStyle`
     body {
         margin:0;
         padding:0;
         font-family:"Noto Sans KR", sans-serif !important;
+    }
+
+    .fade-out {
+      animation:${fadeOut} 250ms forwards !important;
     }
 `;
 
