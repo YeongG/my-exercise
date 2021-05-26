b3e2d8483045a95dd18fd06fe625ae4fadedb999
YeongG <yeonggil223@gmail.com>
2021-05-26 23:41:46 +0900

    [CREATE] assets index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as weightIcon } from "./weightIcon.svg";
+export { default as fatIcon } from "./fatIcon.svg";
+export { default as muscleIcon } from "./muscleIcon.svg";
+export { default as visceralFatIcon } from "./visceralFatIcon.svg";
