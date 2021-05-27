49027bf9fdbcd94816dab61af2e032830c7fb997
YeongG <yeonggil223@gmail.com>
2021-05-27 23:05:10 +0900

    [MODIFY] assets index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,6 @@ export { default as weightIcon } from "./weightIcon.svg";
 export { default as fatIcon } from "./fatIcon.svg";
 export { default as muscleIcon } from "./muscleIcon.svg";
 export { default as visceralFatIcon } from "./visceralFatIcon.svg";
+export { default as pushUpIcon } from "./pushUpIcon.svg";
+export { default as pullUpIcon } from "./pullUpIcon.svg";
+export { default as crunchIcon } from "./crunchIcon.svg";
