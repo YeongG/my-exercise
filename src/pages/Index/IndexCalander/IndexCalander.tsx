fe0206109bbac4f270b0cc83db56246048a21504
YeongG <yeonggil223@gmail.com>
2021-05-27 23:12:03 +0900

    [MODIFY] Index subPage

--- src/pages/Index/IndexCalander/IndexCalander.tsx
+++ src/pages/Index/IndexCalander/IndexCalander.tsx
@@ -4,18 +4,21 @@ import useCalander from "../../../lib/hooks/useCalander";
 import { DateState } from "../../../lib/payloads/default";
 
 interface Props {
+  onChangeMonth?: () => void;
   onClickDate: (date: DateState) => void;
 }
 
-const IndexCalander: FC<Props> = ({ onClickDate }) => {
+const IndexCalander: FC<Props> = ({ onClickDate, onChangeMonth }) => {
   const [date, setDate, changeMonth] = useCalander();
 
   const leftClickHandler = useCallback(() => {
+    if (onChangeMonth) onChangeMonth();
     changeMonth(-1);
-  }, []);
+  }, [onChangeMonth]);
   const rightClickHandler = useCallback(() => {
+    if (onChangeMonth) onChangeMonth();
     changeMonth(1);
-  }, []);
+  }, [onChangeMonth]);
   return (
     <Calander
       title="학사일정"
--- src/pages/Index/IndexDetailExercise/IndexDetailExercise.tsx
+++ src/pages/Index/IndexDetailExercise/IndexDetailExercise.tsx
@@ -1,15 +1,67 @@
-import { FC } from "react";
-import { DetailExercise } from "../../../components";
+import { FC, useCallback, useState } from "react";
+import * as S from "../styles";
+import { DetailExercise, HealthRecorder, InBody } from "../../../components";
 import { DateState } from "../../../lib/payloads/default";
-import { getHealthData } from "../../../lib/utils";
+import { getHealthData, saveHealthData } from "../../../lib/utils";
+import { HealthData, HealthRecorderState } from "../../../lib/payloads/health";
 
 interface Props {
   selectDate: DateState;
 }
 
 const IndexDetailExercise: FC<Props> = ({ selectDate }) => {
+  const [, setReload] = useState<boolean>(false);
   const data = getHealthData(selectDate);
-  return <>{data ? <DetailExercise {...data} /> : ""}</>;
+
+  const submitHealth = useCallback(
+    (data: HealthRecorderState) => {
+      const key: string = `${selectDate.year}-${selectDate.month}-${selectDate.date}`;
+
+      const {
+        crunch,
+        pushUp,
+        fatPercentage,
+        pullUp,
+        visceralFat,
+        muscleMass,
+        weight,
+      } = data;
+      const healthData: HealthData = {
+        exercise: {
+          crunch,
+          pullUp,
+          pushUp,
+        },
+        health: {
+          fatPercentage,
+          weight,
+          muscleMass,
+          visceralFat,
+        },
+      };
+
+      saveHealthData(key, healthData);
+      setReload((prev) => !prev);
+    },
+    [selectDate]
+  );
+
+  return (
+    <>
+      {data ? (
+        <S.DetailInfoWrap>
+          <InBody {...data.health} />
+          <S.DetailExerciseWrap>
+            <DetailExercise {...data.exercise} />
+          </S.DetailExerciseWrap>
+        </S.DetailInfoWrap>
+      ) : (
+        <S.HealthRecorderWrap>
+          <HealthRecorder onSubmitHealth={submitHealth} />
+        </S.HealthRecorderWrap>
+      )}
+    </>
+  );
 };
 
 export default IndexDetailExercise;
