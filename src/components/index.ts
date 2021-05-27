13aab7c983f8691189ac81d252810040004b0da8
YeongG <yeonggil223@gmail.com>
2021-05-27 23:05:21 +0900

    [MODIFY] components index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,7 @@
 export { default as Calander } from "./Calander/Calander";
 export { default as DetailExercise } from "./DetailExercise/DetailExercise";
+export { default as InBody } from "./InBody/InBody";
+export { default as ImageTextBox } from "./ImageTextBox/ImageTextBox";
+export { default as ExerciseCount } from "./ExerciseCount/ExerciseCount";
+export { default as HealthRecorder } from "./HealthRecorder/HealthRecorder";
+export { default as RecorderInput } from "./RecorderInput/RecorderInput";
